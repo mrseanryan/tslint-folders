c9307c0356d78201c7d313d181a00b4c26d8017c
Sean Ryan <sean.ryan@mendix.com>
2018-12-17 16:38:50 +0100

    Add disabled test config for karma, jasmine tests

--- src/model/DisabledTestRuleConfig.ts
+++ src/model/DisabledTestRuleConfig.ts
@@ -5,7 +5,7 @@ export type DisabledTestRuleConfig = {
 
 export function getDefaultDisabledTestRuleConfig(): DisabledTestRuleConfig {
   return {
-    ban: ["describe.only", "it.only", "describe.skip", "it.skip"],
-    includePaths: [".spec.ts", ".spec.tsx", ".ispec.ts", ".ispec.tsx"]
+    ban: ["describe.only", "it.only", "describe.skip", "it.skip", "fdescribe", "fit", "xit", "xdescribe"],
+    includePaths: [".spec.ts", ".spec.tsx", ".ispec.ts", ".ispec.tsx", ".spec.karma.tsx"]
   };
 }

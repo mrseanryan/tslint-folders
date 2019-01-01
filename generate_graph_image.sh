178ecb2d7ba7e4c063413ee0aae56c37bdef0535
sean <str_ie@yahoo.co.uk>
2019-01-01 19:40:20 +0100

    fix: Generate docs script did not work on Linux (Ubuntu)

--- generate_graph_image.sh
+++ generate_graph_image.sh
@@ -2,19 +2,19 @@
 
 # assumption: the machine has an environment variable TEMP pointing to a temporary files location.
 
-TEMP_OUT_DIR=$TEMP
+TEMP_OUT_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'temp'`
 
 OUT_DOT_PATH=$TEMP_OUT_DIR/tslint-folders-docs.dot;
 OUT_IMAGE_PATH=$TEMP_OUT_DIR/tslint-folders-docs.svg;
 
 PATH_TO_TSLINT_JSON=$1;
 
-ECHO yarn docs ...;
+echo yarn docs ...;
 yarn --silent docs $PATH_TO_TSLINT_JSON Dot -outpath=$OUT_DOT_PATH "$2" "$3" "$4" "$5" "$6" "$7"
 
-ECHO dot ...;
+echo dot ...;
 dot $OUT_DOT_PATH -Tsvg -o $OUT_IMAGE_PATH;
 
-ECHO graph image is at $OUT_IMAGE_PATH;
+echo graph image is at $OUT_IMAGE_PATH;
 
-ECHO [done];
+echo [done];

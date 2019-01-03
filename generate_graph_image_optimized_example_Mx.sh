306a237e26593bb3b265086ff1610c3cb6b87876
str <str_ie@yahoo.co.uk>
2019-01-03 19:42:07 +0100

    docs: Move TODOs to Wiki

--- generate_graph_image_optimized_example_Mx.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ECHO Running with extra 'optimization' options to filter some items from the *Mx* diagram
-
-./generate_graph_image.sh  /tmp/tslint.tslint-folders.mx.json  -importBlacklist=mendixmodelsdk -showImportAnyAsNodeNotEdges -clusterFromTslintJson

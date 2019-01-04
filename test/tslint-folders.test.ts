ee8f5110459210f5d843a802e5c07efc88eb4f08
str <str_ie@yahoo.co.uk>
2019-01-04 20:12:39 +0100

    chore: Rename test suite to match this project.

--- test/tslint-folders.test.ts
+++ test/tslint-folders.test.ts
@@ -20,7 +20,7 @@ class ConsoleLogger {
 
 const MARKUP_FILE_EXTENSION = ".lint";
 
-describe("tslint-ban-snippets tests", () => {
+describe("tslint-folders tests", () => {
     it("works if true is truthy", () => {
         expect(true).toBeTruthy();
     });
